25a2368338c350300607adb2c27ffc681f128a84
alpeshgaunker <alpeshgaunker08@gmail.com>
2017-06-08 16:32:26 +0530

    Add files via upload

---
+++ src/StartTest.java
@@ -0,0 +1,14 @@
+package exam;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StartTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
---
+++ src/TestModelTest.java
@@ -0,0 +1,14 @@
+package exam;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestModelTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
